7c907849dc905881c8c57e21271a08d4f34142c2
xszxc <2112067692@qq.com>
2022-10-27 18:21:21 +0800

    LeetCode

---
+++ LeetCode/test5/test5.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+/*
+给你一个字符串 s，找到 s 中最长的回文子串。
+使用中心扩散
+2022年10月26日21:55:01
+*/
+
+func main() {
+	s := "abab"
+	fmt.Println(longestPalindrome(s))
+}
+
+func longestPalindrome(s string) string {
+	if s == "" {
+		return ""
+	}
+
+	strLen := len(s)
+	left := 0    //左扩散的边界下标
+	right := 0   //右扩散的边界下标
+	len := 0     //当前回文串的长度
+	maxLen := 0  //最大回文串长度
+	maxLeft := 0 //最大回文串的左边界
+
+	for i := 0; i < strLen; i++ {
+		left = i - 1
+		right = i + 1
+		//由中心开始左扩散，如果遇到与中心字符不相等的就退出
+		for left >= 0 && (s[left] == s[i]) {
+			left--
+		}
+		//由中心开始右扩散，如果遇到与中心字符不相等的就退出
+		for right < strLen && (s[right] == s[i]) {
+			right++
+		}
+		//由当前的left和right所在位置分别同时左右扩散，如果在扩散过程中两个字符不相等就退出
+		for left >= 0 && right < strLen && (s[left] == s[right]) {
+			left--
+			right++
+		}
+		//最后退出时的下标肯定是不符合条件的，所以要往前复原一次
+		left++
+		right--
+		len = right - left + 1
+		//如果当前len为最大值，就覆盖maxLen和maxLeft的值
+		if len > maxLen {
+			maxLen = len
+			maxLeft = left
+		}
+	}
+
+	return s[maxLeft : maxLeft+maxLen]
+}
---
+++ LeetCode/test6/test6.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+/*
+给你一个字符串 s，找到 s 中最长的回文子串。
+使用动态规划
+*/
+
+func main() {
+	s := "abab"
+	fmt.Println(longestPalindrome(s))
+}
+
+func longestPalindrome(s string) string {
+	//空串或长度为1，直接返回s
+	if s == "" || len(s) == 1 {
+		return s
+	}
+	strLen := len(s)
+	maxLen := 0                  //最大回文串长度
+	maxLeft := 0                 //最大回文串左边界下标
+	maxRight := 0                //最大回文串右边界下标
+	dp := make([][]bool, strLen) //dp[l][r]表示下标从l到到r的字符串是否是回文串
+	for i := 0; i < strLen; i++ {
+		dp[i] = make([]bool, strLen)
+	}
+	//长度为1的字符串都是回文串
+	for i := 0; i < strLen; i++ {
+		dp[i][i] = true
+	}
+	//从r=1开始循环，因为一定有r>l
+	for r := 1; r < strLen; r++ {
+		for l := 0; l < r; l++ {
+			//当前左边界的字符等于右边界的字符,并且从l+1到r-1的字符串为回文串时（特殊情况为当前的子串长度小于等于3）,才有dp[l][r]=true
+			if s[l] == s[r] && (r-l <= 2 || dp[l+1][r-1]) {
+				dp[l][r] = true
+
+				if r-l+1 > maxLen {
+					maxLen = r - l + 1
+					maxLeft = l
+					maxRight = r
+				}
+			}
+		}
+	}
+	return s[maxLeft : maxRight+1]
+
+}
