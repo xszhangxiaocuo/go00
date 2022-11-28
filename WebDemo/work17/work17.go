4d8c7c155dea31df76ff92904f1d5a1d23feddcf
xszxc <2112067692@qq.com>
2022-11-28 23:20:12 +0800

    WebDemo

--- WebDemo/work17/work17.go
+++ WebDemo/work17/work17.go
@@ -28,13 +28,22 @@ func mid1() gin.HandlerFunc {
 
 }
 
+func mid2() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		fmt.Println("mid2 in")
+		c.Next()
+		fmt.Println("mid2 out")
+	}
+}
+
 func main() {
 	//新建一个路由，这个路由没有任何中间件
 	r := gin.New()
-	//为全部路由注册全局中间件
-	r.Use(mid1())
+	//为全部路由注册多个全局中间件
+	r.Use(mid1(), mid2())
 	r.GET("/", func(c *gin.Context) {
 		name := c.MustGet("name").(string) //从上下文取值
 		c.JSON(http.StatusOK, gin.H{"msg": name})
 	})
+	r.Run(":9090")
 }
